87b6effd4c2e2e54c4f4be405be4c542d234a61c
Vincent Desjardins <vdesjardins@gmail.com>
2022-06-17 07:44:49 -0400

    ci: add nix config

--- shell.nix
+++ shell.nix
@@ -1,23 +1,5 @@
-let
-  fetchNixpkgs = fetchTarball "https://github.com/nixos/nixpkgs/archive/6bd1daaf0fe8190a48ac5d27028ef8bed3891ec7.tar.gz";
-in
-{ pkgs ? import fetchNixpkgs { } }:
-
-with pkgs;
-
-mkShell {
-  buildInputs = [
-    go
-    gopls
-    kustomize
-    kubebuilder
-    kind
-    awscli2
-    kubectl
-    kubernetes-helm
-    gnumake
-    jq
-    envsubst
-    chart-testing
-  ];
-}
+(
+  import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
+    src = builtins.fetchGit ./.;
+  }
+).shellNix
